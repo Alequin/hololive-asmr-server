412d6d0235c13c72afd406ef699201e0bdb7baa6
Alequin <j-cox03@hotmail.com>
2022-04-26 20:49:26 +0100

    added toad video to allow list

--- src/store-video-details/get-asmr-videos-in-playlist.js
+++ src/store-video-details/get-asmr-videos-in-playlist.js
@@ -1,20 +1,25 @@
 import { getVideosInPlaylist } from "./get-videos-in-playlist.js";
 
-export const getAsmrVideosInPlaylist = async (channelUploadsPlaylistId, nextPageToken = null) => {
+export const getAsmrVideosInPlaylist = async (
+  channelUploadsPlaylistId,
+  nextPageToken = null
+) => {
   const response = await getVideosInPlaylist({
     playlistId: channelUploadsPlaylistId,
     nextPageToken,
   });
 
   return {
-    videos: response.items.filter((video) => isASMRVideo(video) || isVideoInAllowList(video)),
+    videos: response.items.filter(
+      (video) => isASMRVideo(video) || isVideoInAllowList(video)
+    ),
     nextPageToken: response.nextPageToken,
   };
 };
 
 const isASMRVideo = (video) => /asmr/i.test(video.snippet.title);
 
-// A list of video to include even if they done fit the rules in `isASMRVideo`
-export const VIDEO_ID_ALLOW_LIST = ["_7vOimsaTWI"];
+// A list of video to include even if they don't fit the rules in `isASMRVideo`
+export const VIDEO_ID_ALLOW_LIST = ["_7vOimsaTWI", "3M0sVzE9oKI"];
 const isVideoInAllowList = (video) =>
   VIDEO_ID_ALLOW_LIST.includes(video.snippet.resourceId.videoId);
